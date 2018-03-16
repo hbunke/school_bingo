c2f7def050ee0f2acd0be2c9d3c094a74b2c38d4
Hendrik Bunke <bunke@hbxt.org>
2018-03-16 17:55:57 +0100

    aim to integrate underscore.js
    (links)

--- bingo.js
+++ bingo.js
@@ -1,3 +1,6 @@
+/* Hendrik Bunke, 2018-03-16 */
+/* Bingo with subject terms */
+
 "use_strict";
 
 
@@ -8,7 +11,8 @@ var terms_done = [];
 
 function random_term () {
     var tc = document.getElementById("termcontainer");
-    var term = terms[Math.floor(Math.random() * terms.length)];
+    var term = terms[Math.floor(Math.random() * terms.length)]; /* underscore has a shuffle function: http://underscorejs.org/#shuffle; perhaps better */
+
     var index = term.indexOf(term);
     terms.splice(index, 1);
     terms_done.push(term);
@@ -16,6 +20,9 @@ function random_term () {
     console.log(term);
     console.log(terms);
     console.log(terms_done);
+    /* use _.isEmpty() to check if list is empty
+     * http://underscorejs.org/#isEmpty */
+
 };
 
 
