56c35f31f8f5f486fc2219302e24a6a8718779a8
Hendrik Bunke <bunke@hbxt.org>
2018-03-28 12:48:53 +0200

    first testable prototype

--- bingo.js
+++ bingo.js
@@ -3,27 +3,85 @@
 
 "use_strict";
 
-
-var terms = ["Mindestlohn", "Ludwig Erhard", "Systemkonkurrenz"];
-var terms_done = [];
-
-
+var terms = [
+    "Mindestlohn", 
+    "Ludwig Erhard", 
+    "Systemkonkurrenz", 
+    "Markt", 
+    "Monopole", 
+    "ordoliberal",
+    "freie Marktwirtschaft",
+    "Raubtierkapitalismus",
+    "Wirtschaftsordnung",
+    "Adam Smith",
+    "Gewinn",
+    "Wettbewerb",
+    "Gesetze",
+    "soziale Ungerechtigkeit",
+    "soziale Gerechtigkeit",
+    "Alfred Müller-Armack",
+    "Privateigentum an Produktionsmitteln",
+    "Schutzfunktion des Staates",
+    "Sozialgesetzgebung",
+    "sozialer Frieden",
+    "Sozialversicherungen",
+    "Kündigungsschutz",
+    "Mitbestimmung",
+    "Jugendarbeitsschutz",
+    "Verbot von Kinderarbeit",
+    "Integration",
+    "Walter Eucken",
+    "Freiburger Schule",
+    "Regulierung",
+    "Arbeitnehmerschutz",
+    "Gleichberechtigung am Arbeitsplatz",
+    "staatliche Kontrollen",
+    "Ausbeutung",
+    "Arbeitsunfähigkeit",
+    "Tarifautonomie",
+    "Streikrecht",
+    "Morgenthau-Plan",
+    "Marshall-Plan",
+    "Planwirtschaft",
+    "Marshall-Plan",
+    "Gemeineigentum",
+    "Ahlener Programm",
+    "Kapitalismus",
+    "Sozialismus",
+    "historische Erfahrungen",
+    "schneller Wiederaufbau",
+    "Recht auf Eigentum",
+    "sozialer Ausgleich",
+    "Bundesrepublik Deutschland",
+    "Wirtschaftswunder",
+    "Arbeitszeitregulierung"
+]; 
 
 function random_term () {
     var tc = document.getElementById("termcontainer");
-    var term = terms[Math.floor(Math.random() * terms.length)]; /* underscore has a shuffle function: http://underscorejs.org/#shuffle; perhaps better */
+    var tdc = document.getElementById("done_terms");
 
-    var index = term.indexOf(term);
-    terms.splice(index, 1);
-    terms_done.push(term);
-    tc.innerHTML = term;
+    if (_.isEmpty(terms)) {
+        tc.innerHTML = "Keine Begriffe mehr...";
+    }
+    else {
+        var term = _.sample(terms);
+        tc.innerHTML = term;
+        terms = _.reject(terms, function(t){ return t === term; });
+        tdc.innerHTML += term + "<br />";
+
+    }
+
+    /* DEBUG 
     console.log(term);
     console.log(terms);
     console.log(terms_done);
-    /* use _.isEmpty() to check if list is empty
-     * http://underscorejs.org/#isEmpty */
-
+    */
+        
 };
+
+
+
 
 
 
